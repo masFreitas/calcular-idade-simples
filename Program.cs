b4e916049ed8bc5629462cc5fef38ef91d89c801
Mateus Arthur da Silva de Freitas <65245536+masFreitas@users.noreply.github.com>
2020-12-16 20:45:22 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ExercicioDois
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int anoatual, anonasc, resultado;
+            Console.WriteLine("Informe o ano atual (Ex: 2020):");
+            anoatual = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe o ano de seu nascimento (Ex: 1998):");
+            anonasc = int.Parse(Console.ReadLine());
+
+            resultado = anoatual - anonasc;
+
+            if (resultado >=18)
+            {
+                Console.WriteLine("O usuário tem {0} anos - É de maior", resultado);
+            }
+            else if (resultado <= -1)
+            {
+                Console.WriteLine("Idade inválida");
+            }
+
+            else
+            {
+                Console.WriteLine("O usuário tem {0} anos - Não é de maior", resultado);
+            }
+
+        }
+    }
+}
